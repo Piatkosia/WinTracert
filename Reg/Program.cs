1909be1eefcd68d42e89d3fb8182f23164c4a992
Angelika Maria Piątkowska (piatkosia) <piatkosia.apt@interia.pl>
2018-10-09 21:06:44 +0200

    Add registry viewer, change tracert model to INotifyPropertyChange

---
+++ Reg/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace Reg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey rk = Registry.LocalMachine;
+            GetKeys(rk);
+        }
+
+        private static void GetKeys(RegistryKey rk)
+        {
+            String[] names = rk.GetSubKeyNames();
+            Console.WriteLine("Subkeys of " + rk.Name);
+            Console.WriteLine("-----------------------------------------------");
+            foreach (String s in names)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- WinTracert/TracertModel.cs
+++ WinTracert/TracertModel.cs
@@ -12,21 +12,176 @@ namespace WinTracert
     public class TracertModel: INotifyPropertyChanged
     {
         public static string commandName = "tracert.exe";
-        public bool DParamIsSet { get; set; }
-        public bool HParamIsSet { get; set; }
-        public uint MaxHops { get; set; }
-        public bool JParamIsSet { get; set; }
-        public string HostList { get; set; } 
-        public bool WParamIsSet { get; set; }
-        public uint Timeout { get; set; }
-        public bool RParamIsSet { get; set; }
-        public bool SParamIsSet { get; set; }
-        public string SrcAddr { get; set; }
-        public bool FourParamIsSet { get; set; }
-        public bool SixParamIsSet { get; set; }
-        public string TargetName { get; set; }
-        public string ErrorState { get; internal set; }
+
+        public bool DParamIsSet
+        {
+            get => _dParamIsSet;
+            set
+            {
+                if (value == _dParamIsSet) return;
+                _dParamIsSet = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool HParamIsSet
+        {
+            get => _hParamIsSet;
+            set
+            {
+                if (value == _hParamIsSet) return;
+                _hParamIsSet = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public uint MaxHops
+        {
+            get => _maxHops;
+            set
+            {
+                if (value == _maxHops) return;
+                _maxHops = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool JParamIsSet
+        {
+            get => _jParamIsSet;
+            set
+            {
+                if (value == _jParamIsSet) return;
+                _jParamIsSet = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string HostList
+        {
+            get => _hostList;
+            set
+            {
+                if (value == _hostList) return;
+                _hostList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool WParamIsSet
+        {
+            get => _wParamIsSet;
+            set
+            {
+                if (value == _wParamIsSet) return;
+                _wParamIsSet = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public uint Timeout
+        {
+            get => _timeout;
+            set
+            {
+                if (value == _timeout) return;
+                _timeout = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool RParamIsSet
+        {
+            get => _rParamIsSet;
+            set
+            {
+                if (value == _rParamIsSet) return;
+                _rParamIsSet = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool SParamIsSet
+        {
+            get => _sParamIsSet;
+            set
+            {
+                if (value == _sParamIsSet) return;
+                _sParamIsSet = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string SrcAddr
+        {
+            get => _srcAddr;
+            set
+            {
+                if (value == _srcAddr) return;
+                _srcAddr = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool FourParamIsSet
+        {
+            get => _fourParamIsSet;
+            set
+            {
+                if (value == _fourParamIsSet) return;
+                _fourParamIsSet = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool SixParamIsSet
+        {
+            get => _sixParamIsSet;
+            set
+            {
+                if (value == _sixParamIsSet) return;
+                _sixParamIsSet = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string TargetName
+        {
+            get => _targetName;
+            set
+            {
+                if (value == _targetName) return;
+                _targetName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ErrorState
+        {
+            get => _errorState;
+            internal set
+            {
+                if (value == _errorState) return;
+                _errorState = value;
+                OnPropertyChanged();
+            }
+        }
+
         private string _outText = "";
+        private bool _dParamIsSet;
+        private bool _hParamIsSet;
+        private uint _maxHops;
+        private bool _jParamIsSet;
+        private string _hostList;
+        private bool _wParamIsSet;
+        private uint _timeout;
+        private bool _rParamIsSet;
+        private bool _sParamIsSet;
+        private string _srcAddr;
+        private bool _fourParamIsSet;
+        private bool _sixParamIsSet;
+        private string _targetName;
+        private string _errorState;
 
         public string OutText
         {
